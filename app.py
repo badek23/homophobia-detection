69bf271040bcc076a0bd4dda437347275e1fb228
Kate <kate.bade@student.ie.edu>
2024-06-20 12:11:36 +0200

    app

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Sidebar
+
+# Main content
+st.title('Homophobia Detector')
+st.markdown(
+    """
+    This app will detect whether Tweets are homophobic. As hate speech spreads, our aim is to be able to more reliably detect
+     when it is present on social media. To test the Tweet, input it into the text box below.
+    """
+)
+
+Tweet = st.text_input("Copy the Tweet here:")
+
